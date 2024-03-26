05eedbb95ef73142526a149a240e026f2565b04b
tvsmsa <tmmelekhin@gmail.com>
2024-03-26 20:11:36 +0500

    Улучшение модели и администрация

--- ithinkbooks/catalogue/admin.py
+++ ithinkbooks/catalogue/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import AppConfig
+from catalogue.models import Products
 
 # Register your models here.
+admin.site.register(Products)
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'book'
+    verbose_name = 'Книга'
--- ithinkbooks/catalogue/models.py
+++ ithinkbooks/catalogue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 from multiselectfield import MultiSelectField
 
 # Create your models here.
@@ -7,24 +8,29 @@ class Products(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     author = models.CharField(max_length=150, null=True, verbose_name='Автор')
     publisher = models.CharField(max_length=150, null=True, verbose_name='Издательство')
-    year = models.IntegerField(max_length=4, null=True, verbose_name='Год')
-    number_of_pages = models.IntegerField(max_length=10, null=True, verbose_name='Количество страниц')
+    year = models.IntegerField(null=True, verbose_name='Год')
+    number_of_pages = models.IntegerField( null=True, verbose_name='Количество страниц')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='catalogue_images', blank=True, null=True, verbose_name='Изображение')
+    #image = models.ImageField(upload_to='catalogue_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     #Типы книг
     class BookTypes(models.TextChoices): #Переделать позже, может очень просто совпадать
-        FRONTEND = "FE",_("Frontend")
-        BACKEND = "BE",_("Backend")
-        DESIGN = "DE",_("Дизайн")
-        GAMEDEV = "GD",_("Gamedev")
-        JAVASCRIPT = "JS",_("JavaScript")
-        PYTHON = "PY",_("Python")
-        CLANGUAGE = "CL",_("C")
+        FRONTEND = "FE","Frontend"
+        BACKEND = "BE","Backend"
+        DESIGN = "DE","Дизайн"
+        GAMEDEV = "GD","Gamedev"
+        COMPSCI = "CS","Computer Science" #Разбить на несколько
+        JAVASCRIPT = "JS","JavaScript"
+        JAVA = "J","Java"
+        PYTHON = "PY","Python"
+        CSHARP = "C#","C#"
+        CLANGUAGE = "C","C"
+        CPLUS = "C++","C++"
+        SQL = "SQL","SQL"
+        OTHER = "OT","Другое"
     #Тип книги, максимум четыре типа
-    book_type = MultiSelectField( max_length=6, choices=BookTypes, max_choices = 4 ,default=BookTypes.FRONTEND,verbose_name='Темы')
-
+    book_type = MultiSelectField( max_length=20, choices=BookTypes, max_choices = 4, default=BookTypes.FRONTEND,verbose_name='Темы')
     #Типы переплета
     class BookBinding(models.TextChoices):
         SOFTCOVER = "Мягкий переплет"
@@ -38,9 +44,9 @@ class Products(models.Model):
     #Выбор перевода
     translator_choice = models.CharField (max_length=30, choices = Translator, default=Translator.TRANSLATE, verbose_name='Переводчик')
     class Meta:
-        db_table = 'product'
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        db_table = 'book'
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
 
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
