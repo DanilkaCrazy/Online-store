4ab6880a949b0895e3ec7387bb5fc17670e62e1a
tvsmsa <tmmelekhin@gmail.com>
2024-03-26 10:07:41 +0500

    Starting catalogue

---
+++ ithinkbooks/catalogue/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from catalogue import views
+
+app_name = 'catalogue'
+urlpatterns = [
+    #path('',views.catalogue, name='catalogue'),
+    #path('product/',views.product,name='product')
+]
