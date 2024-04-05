3412fbc07928e3e14eed7a234fee8f75c728a5f6
tvsmsa <tmmelekhin@gmail.com>
2024-04-05 21:02:36 +0500

    Getting data from backend

--- ithinkbooks/frontend/src/components/collections/ThemeCollection.tsx
+++ ithinkbooks/frontend/src/components/collections/ThemeCollection.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { books } from '../mock/mock';
 import Theme from '../Theme';
+import axios from 'axios';
 
 const ThemeCollection: React.FC<{theme: Theme}> = ({theme}) => {
   const themeBooks = books.filter((book) => book.themes.includes(theme));
-
+  const GetData = () => {
+    axios.get('http://127.0.0.1:8000/products').then((res)=>{
+      console.log(res.data)
+    })}
+  useEffect(()=>{
+    GetData()
+  })
   return (
     <>
       <h1>{theme.name}</h1>
