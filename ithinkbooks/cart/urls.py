ce34143cd7f231214c163aa2edffac684321204a
tvsmsa <tmmelekhin@gmail.com>
2024-04-20 18:19:56 +0500

    More cart views

--- ithinkbooks/cart/urls.py
+++ ithinkbooks/cart/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 #from users.views import 
-from cart.views import CartItemsView
+from cart.views import CartItemsView, CartChangeView, CartRemoveView
 
 app_name = 'cart'
 urlpatterns = [
     path('items', CartItemsView.as_view(), name='Cart Items'),
-    #path('logout', UserLogout.as_view(), name='Logout'),
-    #path('login', UserLogin.as_view(), name='Login')
+    path('change', CartChangeView.as_view(), name='Change cart'),
+    path('remove', CartRemoveView.as_view(), name='Remove item from cart')
 ]
