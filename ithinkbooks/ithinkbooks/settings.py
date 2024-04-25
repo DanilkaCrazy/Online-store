25de4db55a3cbcfa313a661002e70706c3ecfa62
tvsmsa <tmmelekhin@gmail.com>
2024-04-25 16:59:14 +0500

    Moved to PSQL

--- ithinkbooks/ithinkbooks/settings.py
+++ ithinkbooks/ithinkbooks/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'ithinkbooks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ithinkbooks',
+        'USER': 'Ithink',
+        'PASSWORD': 'root1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
